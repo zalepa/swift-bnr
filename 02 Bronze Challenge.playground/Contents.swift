68146d13d3cd5ca1112aaede7ed3b81eb1037da2
George Zalepa <george.zalepa@gmail.com>
2017-08-28 07:40:08 -0400

    Finish chapter 2

---
+++ 02 Bronze Challenge.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+
+let townName: String = "Knowhere"
+
+var unemploymentLevel: Double = 3.6
+
+let townDescription: String = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights. It has an unemployment rate of \(unemploymentLevel)%."
+
+print(townDescription)
---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+
+let townName: String = "Knowhere"
+let townDescription: String = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights."
+
+print(townDescription)
